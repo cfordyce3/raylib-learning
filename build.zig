0f291c038bdddda80ab07323be555a0db9c69275
Chas <chasmo123@gmail.com>
2026-03-09 22:36:58 -0700

    getting zig going

---
+++ build.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const exe = b.addExecutable(.{ .name = "rl", .root_module = b.createModule(.{ .root_source_file = b.path("src/rl.zig"), .target = b.graph.host }) });
+    b.installArtifact(exe);
+}
